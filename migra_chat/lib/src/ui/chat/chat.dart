db66b2d6430754ae7f1a3754d1fe88c63d707f95
jonathan <jortega@pivotport.com>
2024-04-10 01:25:02 +0000

    moved resources out of app dir

---
+++ migra_chat/lib/src/ui/chat/chat.dart
@@ -0,0 +1,1 @@
+import 'package:langchain/langchain.dart';
--- model_playground/model_testing/android/app/src/main/kotlin/com/example/model_testing/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.model_testing
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
